0e6edd8ae739cb5028b157ae7c44c8e3508a6da0
Steven Braham <steven@braham.biz>
2016-01-11 21:07:17 +0100

    base layout

--- source/js/mainPage.js
+++ source/js/mainPage.js
@@ -1,1 +1,10 @@
-console.log(postalCodes);
+$(document).ready(function() {
+    var form = $(".login-form");
+    form.css({
+        opacity: 1,
+        "-webkit-transform": "scale(1)",
+        "transform": "scale(1)",
+        "-webkit-transition": ".5s",
+        "transition": ".5s"
+    });
+});
