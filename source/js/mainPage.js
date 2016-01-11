28c145261717356839970449eceb563ecd2f64fb
Steven Braham <steven@braham.biz>
2016-01-11 21:17:34 +0100

    search implemented

--- source/js/mainPage.js
+++ source/js/mainPage.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-    var form = $(".login-form");
-    form.css({
+    $(".login-form").css({
         opacity: 1,
         "-webkit-transform": "scale(1)",
         "transform": "scale(1)",
         "-webkit-transition": ".5s",
         "transition": ".5s"
     });
+    $("#searchForm").on("input", function() {
+        //search function
+        var searchTerm = parseInt($(this).val());
+        if (searchTerm in postalCodes) {
+            $("#searchResult").text(postalCodes[searchTerm]);
+        } else {
+            $("#searchResult").text("Geen resultaten");
+        }
+    })
 });
