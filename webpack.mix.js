6d21460c1391e40417b99679cd3535311980814b
Steven Braham <steven@braham.biz>
2021-11-14 21:01:43 +0100

    added latest webpack and laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
-const mix = require('laravel-mix');
-require('laravel-mix-purgecss');
-
-mix.setPublicPath('build/');
+const mix = require("laravel-mix");
+const path = require("path");
+require("laravel-mix-purgecss");
+mix.setPublicPath("build/");
 mix.ts("src/js/app.ts", "./build/js");
 mix.copy("src/index.html", "./build/index.html");
 mix.copy("src/favicon.png", "./build/favicon.png");
 mix.sass("src/sass/style.scss", "./build/css").purgeCss({
-    content: [path.join(__dirname, 'build/*.html')],
+    content: [path.join(__dirname, "build/*.html")]
 });
