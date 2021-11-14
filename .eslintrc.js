44f94898139293dbd4c255be6b67d5901b24e879
Steven Braham <steven@braham.biz>
2021-11-14 20:37:27 +0100

    setup prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        node: false,
+        es2021: true
+    },
+    extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+        project: "./tsconfig.json"
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+        "object-shorthand": "off",
+        "@typescript-eslint/indent": ["error", 4],
+        "@typescript-eslint/comma-dangle": "off",
+        "@typescript-eslint/quotes": [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true
+            }
+        ],
+        "prettier/prettier": 2,
+        "no-param-reassign": [2, { props: false }]
+    }
+};
--- src/js/app.ts
+++ src/js/app.ts
@@ -1,18 +1,21 @@
-import postalCodes from "./postalcodeList"
-
-//set year
-document.getElementById("year")!.innerText = new Date().getFullYear().toString();
-
-let inputForm = document.getElementById("postal-form");
-let inputField = document.getElementById("search");
-let resultElement = document.getElementById('searchResult');
-
+import postalCodes from "./postalcodeList";
+// set year
+const yearField = document.getElementById("year");
+if (yearField !== null) {
+    yearField.innerText = new Date().getFullYear().toString();
+}
+const inputForm = document.getElementById("postal-form");
+const inputField = document.getElementById("search");
+const resultElement = document.getElementById("searchResult");
+const c = true;
 function searchPostcode() {
-    if (inputField instanceof HTMLInputElement && resultElement instanceof HTMLElement) {
-        let searchTerm = inputField.value;
+    if (
+        inputField instanceof HTMLInputElement &&
+        resultElement instanceof HTMLElement
+    ) {
+        const searchTerm = inputField.value;
         if (searchTerm in postalCodes) {
-            //@ts-ignore()
-            resultElement.innerText = postalCodes[searchTerm];
+            resultElement.innerText = (postalCodes as any)[searchTerm];
         } else {
             resultElement.innerText = "Geen resultaten";
         }
@@ -23,7 +26,6 @@ if (inputForm !== null) {
     inputForm.style.opacity = "1";
     inputForm.style.transform = "scale(1)";
     inputForm.style.transition = ".5s";
-
     inputForm.onsubmit = (e) => {
         e.preventDefault();
         searchPostcode();
